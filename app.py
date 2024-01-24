6e0c46b88d1629bacfbad1cde1f791888a188b8e
RACollins <richardcollins128@mail.com>
2024-01-24 23:51:05 +0900

    Added main() function

--- app.py
+++ app.py
@@ -21,8 +21,12 @@ st.set_page_config(
 )
 
 
-background_img = utils.get_img_as_base64(
+sidebar_img = utils.get_img_as_base64(
     root_dir_path + "/data/app/wp11906650-fantasy-premier-league-wallpapers.jpg"
+)
+
+background_img = utils.get_img_as_base64(
+    root_dir_path + "/data/app/wp2598920-white-wallpaper.jpg"
 )
 
 st.markdown(
@@ -35,11 +39,12 @@ st.markdown(
     }}
 
     [data-testid=stAppViewContainer] {{
-        
+        background-image: url("data:image/png;base64,{background_img}");
+        background-size: cover;
     }}
 
     [data-testid="stSidebar"] {{
-        background-image: url("data:image/png;base64,{background_img}");
+        background-image: url("data:image/png;base64,{sidebar_img}");
         background-size: cover;
     }}
 
@@ -74,6 +79,11 @@ col_name_change_dict = {
     "event_transfers_cost": "Transfer Costs",
     "points_on_bench": "Points on Bench",
 }
+heatmap_colourscale = [
+    [0, "rgba(61,23,90,255)"],
+    [0.35, "rgba(70,160,246,255)"],
+    [1, "rgba(72,250,137,255)"],
+]
 
 
 #################
@@ -97,13 +107,16 @@ def get_league_data(leagueID):
     return league_name, league_df
 
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def get_all_mngrs_all_gws_df(league_df):
     all_gws_url_template = (
         "https://fantasy.premierleague.com/api/entry/{manager_id}/history/"
     )
     all_gws_df_list = []
-    for i, manager_id in enumerate(league_df["ID"]):
+    manager_ids = league_df["ID"].values
+    prog_bar = st.progress(0)
+    percent_complete = st.empty()
+    for i, manager_id in enumerate(manager_ids):
         respose = requests.get(all_gws_url_template.format(manager_id=manager_id))
         all_gws_respose_json = respose.json()
         all_gws_df = pd.DataFrame(all_gws_respose_json["current"])
@@ -111,6 +124,10 @@ def get_all_mngrs_all_gws_df(league_df):
         all_gws_df["Team Name"] = league_df.loc[i, "Team Name"]
         all_gws_df["Manager"] = league_df.loc[i, "Manager"]
         all_gws_df_list.append(all_gws_df)
+        percent_complete.text("{0:.3f} %".format(100 * ((i + 1) / len(manager_ids))))
+        prog_bar.progress((i + 1) / len(manager_ids))
+    prog_bar.empty()
+    percent_complete.empty()
     all_mngrs_all_gws_df = (
         pd.concat(all_gws_df_list)
         .rename(columns=col_name_change_dict)
@@ -160,6 +177,7 @@ def get_picks_and_teams_dfs(league_df, players_df, max_gw):
     league_picks_dict = {}
     manager_ids = league_df["ID"].values
     prog_bar = st.progress(0)
+    percent_complete = st.empty()
     for i, manager_id in enumerate(manager_ids):
         league_picks_dict[manager_id] = []
         for j, gw in enumerate(range(1, max_gw + 1)):
@@ -190,8 +208,15 @@ def get_picks_and_teams_dfs(league_df, players_df, max_gw):
                     + picks_df["gw"].astype(str)
                 ).values
             )
+            percent_complete.text(
+                "{0:.3f} %".format(
+                    100 * (((i * max_gw) + (j + 1)) / (len(manager_ids) * max_gw))
+                )
+            )
             prog_bar.progress(((i * max_gw) + (j + 1)) / (len(manager_ids) * max_gw))
+
     prog_bar.empty()
+    percent_complete.empty()
 
     league_teams_df = pd.concat(league_teams_df_list).reset_index(drop=True)
     league_picks_df = pd.DataFrame(league_picks_dict).rename(
@@ -204,140 +229,155 @@ def get_picks_and_teams_dfs(league_df, players_df, max_gw):
 ### App proper ###
 ##################
 
-st.title("FPL League Dashboard")
-st.subheader("Input your league ID to view various statistics")
 
-leagueID = st.number_input(
-    "League ID", value=None, placeholder="Type your league ID here...", step=1
-)
+def main():
+    st.title("FPL League Dashboard")
+    st.subheader("Input your league ID to view various statistics")
 
-render_elements = False
-if leagueID != None:
-    render_elements = True
-else:
-    league_df = None
-    st.info(
-        "How to find you league ID:  \n"
-        "👉 Login to your FPL account  \n"
-        "👉 Select the 'Leagues & Cups' tab  \n"
-        "👉 Select a league  \n"
-        "👉 Copy ID from the URL  \n"
+    leagueID = st.number_input(
+        "League ID", value=None, placeholder="Type your league ID here...", step=1
     )
 
-##############
-### Render ###
-##############
+    render_elements = False
+    if leagueID != None:
+        render_elements = True
+    else:
+        league_df = None
+        st.info(
+            "How to find you league ID:  \n"
+            "👉 Login to your FPL account  \n"
+            "👉 Select the 'Leagues & Cups' tab  \n"
+            "👉 Select a league  \n"
+            "👉 Copy ID from the URL  \n"
+        )
 
-if render_elements:
-    league_name, league_df = get_league_data(leagueID)
-    all_mngrs_all_gws_df = get_all_mngrs_all_gws_df(league_df)
-    with st.sidebar:
-        tab_headers = {"tab1": "Summary", "tab2": "Season Stats.", "tab3": "Teams"}
-        st.header("Info...")
-        with st.expander(tab_headers["tab1"]):
-            st.write(
-                "A convenient summary table of leaue standings for the current season. "
-                "Not too dissimilar to the summary table on the official app/website."
-            )
-        with st.expander(tab_headers["tab2"]):
-            st.write("Nothing yet...")
-        with st.expander(tab_headers["tab3"]):
-            st.write("Nothing yet...")
-    tab1, tab2, tab3 = st.tabs([tab_headers[k] for k, v in tab_headers.items()])
-    with tab1:
-        st.header(f"{league_name}")
-        max_gw = all_mngrs_all_gws_df["GW"].max()
-        league_df = league_df.merge(
-            all_mngrs_all_gws_df.loc[
-                all_mngrs_all_gws_df["GW"] == max_gw, ["Manager", "Form"]
-            ],
-            how="inner",
-            on="Manager",
-        )
-        st.dataframe(
-            league_df[
-                ["Rank", "Manager", "Team Name", "GW Total", "Total Points", "Form"]
-            ].style.format({"Form": "{:.2f}"}, thousands=","),
-            use_container_width=True,
-            hide_index=True,
-        )
-    with tab2:
-        st.header(f"{league_name}")
-        with st.container(border=True):
-            y_axis_option = st.selectbox(
-                "Pick a Parameter to Plot",
-                [
-                    "Points",
-                    "Total Points",
-                    "Rank",
-                    "Overall Rank",
-                    "Bank",
-                    "Value",
-                    "Form",
-                    "Transfers",
-                    "Transfer Costs",
-                    "Points on Bench",
-                    "Total Transfers",
-                    "Total Transfer Costs",
-                    "Total Points on Bench",
-                ],
-            )
-            gw_range = st.slider("Select Gameweek Range", 1, max_gw, (1, max_gw))
-            fig = px.line(
-                all_mngrs_all_gws_df[
-                    all_mngrs_all_gws_df["GW"].between(gw_range[0], gw_range[1])
-                ],
-                x="GW",
-                y=y_axis_option,
-                color="Manager",
-                color_discrete_sequence=px.colors.qualitative.Plotly,
-                markers=True,
-            )
-            if y_axis_option in ["Rank", "Overall Rank"]:
-                fig.update_yaxes(autorange="reversed")
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-    with tab3:
-        st.header(f"{league_name}")
+    ##############
+    ### Render ###
+    ##############
+
+    if render_elements:
+        league_name, league_df = get_league_data(leagueID)
+        with st.spinner(text="Processing data, might take a while..."):
+            all_mngrs_all_gws_df = get_all_mngrs_all_gws_df(league_df)
         players_df = get_players_df()
+        max_gw = all_mngrs_all_gws_df["GW"].max()
         with st.spinner(text="Processing data, might take a while..."):
             league_teams_df, league_picks_df = get_picks_and_teams_dfs(
                 league_df, players_df, max_gw
             )
-        colorscale = [
-            [0, "rgba(61,23,90,255)"],
-            [0.5, "rgba(70,160,246,255)"],
-            [1, "rgba(72,250,137,255)"],
-        ]
-        with st.container(border=True):
-            gw_type = st.radio(
-                "Choose Gameweek Selection Type",
-                ["Single Gameweek", "Multiple Gameweeks"],
-                captions=[
-                    "Team similarity of a single GW",
-                    "Average similarity over multiple GWs",
+        with st.sidebar:
+            tab_headers = {"tab1": "Summary", "tab2": "Season Stats.", "tab3": "Teams"}
+            st.header("Info...")
+            with st.expander(tab_headers["tab1"]):
+                st.write(
+                    "A convenient summary table of league standings for the current season. "
+                    "Not too dissimilar to the summary table on the official app/website."
+                )
+            with st.expander(tab_headers["tab2"]):
+                st.write(
+                    "A line graph of various statistics for each manager throughout the current season. "
+                    "Pick a parameter to plot and adjust the gameweek range. The graph is interactive too!"
+                )
+            with st.expander(tab_headers["tab3"]):
+                st.write(
+                    "Check which managers have had similar team selections for a given gameweek. "
+                    "Or, select a range of gameweeks to find the average similarity."
+                )
+        tab1, tab2, tab3 = st.tabs([tab_headers[k] for k, v in tab_headers.items()])
+        with tab1:
+            st.header(f"{league_name}")
+            league_df = league_df.merge(
+                all_mngrs_all_gws_df.loc[
+                    all_mngrs_all_gws_df["GW"] == max_gw, ["Manager", "Form"]
                 ],
-                horizontal=True,
+                how="inner",
+                on="Manager",
             )
-
-            if gw_type == "Single Gameweek":
-                gw_range = st.slider(
-                    "Select Gameweek Range", 1, max_gw, 1, key="single_gw"
-                )
-                gw_select_indx = list(range((gw_range - 1) * 15, gw_range * 15))
-            elif gw_type == "Multiple Gameweeks":
-                gw_range = st.slider(
-                    "Select Gameweek Range", 1, max_gw, (1, max_gw), key="multi_gw"
-                )
-                gw_select_indx = list(range((gw_range[0] - 1) * 15, gw_range[1] * 15))
-            else:
-                gw_select_indx = []
-
-            sim_df = utils.jaccard_sim(league_picks_df.iloc[gw_select_indx])
-            fig = px.imshow(
-                sim_df,
-                text_auto=False,
-                aspect="auto",
-                color_continuous_scale=colorscale,
-                labels=dict(x="Manager 1", y="Manager 2", color="Similarity"),
+            st.dataframe(
+                league_df[
+                    ["Rank", "Manager", "Team Name", "GW Total", "Total Points", "Form"]
+                ].style.format({"Form": "{:.2f}"}, thousands=","),
+                use_container_width=True,
+                hide_index=True,
             )
-            st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        with tab2:
+            st.header(f"{league_name}")
+            with st.container(border=True):
+                y_axis_option = st.selectbox(
+                    "Pick a Parameter to Plot",
+                    sorted(
+                        [
+                            "Points",
+                            "Total Points",
+                            "Rank",
+                            "Overall Rank",
+                            "Bank",
+                            "Value",
+                            "Form",
+                            "Transfers",
+                            "Transfer Costs",
+                            "Points on Bench",
+                            "Total Transfers",
+                            "Total Transfer Costs",
+                            "Total Points on Bench",
+                        ]
+                    ),
+                    index=3,
+                )
+                gw_range = st.slider("Select Gameweek Range", 1, max_gw, (1, max_gw))
+                fig = px.line(
+                    all_mngrs_all_gws_df[
+                        all_mngrs_all_gws_df["GW"].between(gw_range[0], gw_range[1])
+                    ],
+                    x="GW",
+                    y=y_axis_option,
+                    color="Manager",
+                    color_discrete_sequence=px.colors.qualitative.Plotly,
+                    markers=True,
+                )
+                if y_axis_option in ["Rank", "Overall Rank"]:
+                    fig.update_yaxes(autorange="reversed")
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        with tab3:
+            st.header(f"{league_name}")
+            # st.dataframe(league_teams_df)
+
+            with st.container(border=True):
+                gw_type = st.radio(
+                    "Choose Gameweek Selection Type",
+                    ["Single Gameweek", "Multiple Gameweeks"],
+                    captions=[
+                        "Team similarity of a single GW",
+                        "Average similarity over multiple GWs",
+                    ],
+                    horizontal=True,
+                )
+
+                if gw_type == "Single Gameweek":
+                    gw_range = st.slider(
+                        "Select Gameweek Range", 1, max_gw, 1, key="single_gw"
+                    )
+                    gw_select_indx = list(range((gw_range - 1) * 15, gw_range * 15))
+                elif gw_type == "Multiple Gameweeks":
+                    gw_range = st.slider(
+                        "Select Gameweek Range", 1, max_gw, (1, max_gw), key="multi_gw"
+                    )
+                    gw_select_indx = list(
+                        range((gw_range[0] - 1) * 15, gw_range[1] * 15)
+                    )
+                else:
+                    gw_select_indx = []
+
+                sim_df = utils.jaccard_sim(league_picks_df.iloc[gw_select_indx])
+                fig = px.imshow(
+                    sim_df,
+                    text_auto=False,
+                    aspect="auto",
+                    color_continuous_scale=heatmap_colourscale,
+                    labels=dict(x="Manager 1", y="Manager 2", color="Similarity"),
+                )
+                st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
+if __name__ == "__main__":
+    main()
